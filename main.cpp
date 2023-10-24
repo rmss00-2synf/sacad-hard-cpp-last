5667ae10fc9665cefc12e67e07f4fd21d6d92141
SYLLA <synf30@gmail.com>
2023-10-24 20:41:58 +0100

    Premier fichier

---
+++ main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,unordered_map<char,int>> board_map;
+        for(auto i = 0; i<9; i++){
+            for(auto j = 0; j<9; j++){
+                char cell = board[i][j];
+                if(cell != '.'){
+		//We create verify and increment together
+                    if(board_map[i][cell]++ > 0 || board_map[j+9][cell]++ > 0 || board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
+                    		}
+            }
+        }
+        return true;
+        
+    }
+};
