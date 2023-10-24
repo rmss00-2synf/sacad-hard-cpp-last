c540e71dba8788fc2f08d69ac467dff07aac8e92
SYLLA <synf30@gmail.com>
2023-10-24 20:44:51 +0100

    Ajout de commentaire

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int,unordered_map<char,int>> board_map;
-        for(auto i = 0; i<9; i++){
-            for(auto j = 0; j<9; j++){
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        unordered_map<int, unordered_map<char, int>> board_map;
+        for (auto i = 0; i < 9; i++)
+        {
+            for (auto j = 0; j < 9; j++)
+            {
                 char cell = board[i][j];
-                if(cell != '.'){
-		//We create verify and increment together
-                    if(board_map[i][cell]++ > 0 || board_map[j+9][cell]++ > 0 || board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
-                    		}
+                if (cell != '.')
+                {
+                    // We create verify and increment together
+
+                    if (board_map[i][cell]++ > 0 || board_map[j + 9][cell]++ > 0 || board_map[27 + (i / 3) * 3 + j / 3][cell]++ > 0)
+                        return false;
+                }
             }
         }
         return true;
-        
     }
 };
+// C'est la solution la plus simple que j'ai pu faire
